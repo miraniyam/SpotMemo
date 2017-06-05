7e4ddd8c0225cc18d16510b02a25ade1f62cea88
Kyeol Han <kyeolhan@Kyeolui-MacBook.local>
2017-06-05 10:44:23 +0900

    위도, 경도로 파일명 저장

--- app/src/main/java/com/example/miranlee/spotmemo/AddVoiceActivity.java
+++ app/src/main/java/com/example/miranlee/spotmemo/AddVoiceActivity.java
@@ -1,10 +1,16 @@
 package com.example.miranlee.spotmemo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
 import android.speech.tts.TextToSpeech;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +25,9 @@ import java.util.Locale;
  * Created by miran lee on 2017-05-14.
  */
 
-public class AddVoiceActivity extends ActionBarActivity implements TextToSpeech.OnInitListener{
+public class AddVoiceActivity extends ActionBarActivity implements TextToSpeech.OnInitListener, LocationListener {
+
+    private LocationManager locationManager;
 
     MediaRecorder recorder = new MediaRecorder();
     TextToSpeech tts;
@@ -27,6 +35,8 @@ public class AddVoiceActivity extends ActionBarActivity implements TextToSpeech.
     Button startbtn;
     Button stopbtn;
     Button backbtn;
+
+    String msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +46,19 @@ public class AddVoiceActivity extends ActionBarActivity implements TextToSpeech.
 
         tts = new TextToSpeech(this, this);
         tts.setLanguage(Locale.KOREA);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 1, (LocationListener) this);
 
         startbtn = (Button)findViewById(R.id.btn_start);
         stopbtn = (Button)findViewById(R.id.btn_stop);
@@ -50,12 +73,12 @@ public class AddVoiceActivity extends ActionBarActivity implements TextToSpeech.
 
             boolean f = nfile.mkdirs();
 
-            String filename = new SimpleDateFormat("yyyy-mm-dd-hh-mm-ss").format(new Date());
+            //String filename = new SimpleDateFormat("yyyy-mm-dd-hh-mm-ss").format(new Date());
 
             recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
             recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
             recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-            recorder.setOutputFile(nfile.getAbsolutePath()+"/"+filename+".3gp");
+            recorder.setOutputFile(nfile.getAbsolutePath()+"/"+msg+".3gp");
             recorder.prepare();
             while(tts.isSpeaking()) {
                 // 안내 음성이 다 끝나고 나야 저장할 것이다!
@@ -87,5 +110,25 @@ public class AddVoiceActivity extends ActionBarActivity implements TextToSpeech.
         Intent i = new Intent(this, AddMemoActivity.class);
         finish();
         startActivity(i);
+    }
+
+    public void onLocationChanged(Location location){
+        msg = ""+location.getLatitude() +" "+location.getLongitude();
+        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
