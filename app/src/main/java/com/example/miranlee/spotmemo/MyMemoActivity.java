84db8065531ee61e68d3f178e197a48a012e34fc
miraniyam <ranz_@naver.com>
2017-06-08 15:44:04 +0900

    내가 남긴 메모에서 클릭 리스너 구현 - 음성메모면 play, 텍스트 메모면 읽어주기

--- app/src/main/java/com/example/miranlee/spotmemo/MyMemoActivity.java
+++ app/src/main/java/com/example/miranlee/spotmemo/MyMemoActivity.java
@@ -1,24 +1,33 @@
 package com.example.miranlee.spotmemo;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
  * Created by miran lee on 2017-05-14.
  */
 
-public class MyMemoActivity extends ActionBarActivity {
+public class MyMemoActivity extends ActionBarActivity implements TextToSpeech.OnInitListener{
 
     ListView listView;
     ArrayAdapter adapter;
+    ArrayList<String> fName;
+    ArrayList<String> fPath;
+    ArrayList<Integer> fType;
+    TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,26 +35,78 @@ public class MyMemoActivity extends ActionBarActivity {
         setContentView(R.layout.activity_mymemo);
         setTitle("내가 남긴 메모");
 
+        tts = new TextToSpeech(this,this);
+
+        fName = new ArrayList<>();
+        fPath = new ArrayList<>();
+        fType = new ArrayList<>();
         listView = (ListView)findViewById(R.id.mlistView);
 
         String ext = Environment.getExternalStorageState();
         if (ext.equals(Environment.MEDIA_MOUNTED)) {
-            ArrayList<String> fName = new ArrayList<>();
             File files = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Documents/SpotMemo/Voice");
             ArrayAdapter<String> filelist = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fName);
             if (files.listFiles().length > 0){
                 for (File file:files.listFiles()){
+                    // 음성 메모이면 0
                     fName.add(file.getName());
+                    fPath.add(file.getAbsolutePath());
+                    fType.add(0);
                 }
             }
             files = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Documents/SpotMemo/Text");
             if (files.listFiles().length > 0){
                 for (File file:files.listFiles()){
+                    // 텍스트 메모이면 1
                     fName.add(file.getName());
+                    fPath.add(file.getAbsolutePath());
+                    fType.add(1);
                 }
             }
-            files = null;
             listView.setAdapter(filelist);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    if(fType.get(position) == 0) {
+                        // 음성 파일이면 재생
+                        MediaPlayer mediaPlayer = new MediaPlayer();
+                        try {
+                            mediaPlayer.setDataSource(fPath.get(position));
+                        }catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        try{
+                            mediaPlayer.prepare();
+                        }catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        mediaPlayer.start();
+                    }else {
+                        // 텍스트 파일이면 tts로 읽어주기
+                        File file = new File(fPath.get(position));
+                        String s = "";
+                        char ch;
+                        int data;
+                        FileReader fr = null;
+                        try {
+                            fr = new FileReader(file);
+                            while((data = fr.read()) != -1) {
+                                ch = (char)data;
+                                s = s+ch;
+                            }
+                            fr.close();
+                        }catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        if(s != "") {
+                            tts.speak(s,TextToSpeech.QUEUE_FLUSH,null);
+                        }else {
+                            tts.speak("파일 내용이 없습니다.",TextToSpeech.QUEUE_FLUSH,null);
+                        }
+                    }
+                }
+            });
         }
     }
 
@@ -53,5 +114,10 @@ public class MyMemoActivity extends ActionBarActivity {
         Intent i = new Intent(this, MainActivity.class);
         finish();
         startActivity(i);
+    }
+
+    @Override
+    public void onInit(int status) {
+
     }
 }
