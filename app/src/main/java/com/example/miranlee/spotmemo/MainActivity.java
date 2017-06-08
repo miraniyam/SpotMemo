880c0f68919a1cb53a3203de6127f28a7daf2fc9
tazozzang <dlwldus718@naver.com>
2017-06-08 22:53:07 +0900

    지도 핑찍기

--- app/src/main/java/com/example/miranlee/spotmemo/MainActivity.java
+++ app/src/main/java/com/example/miranlee/spotmemo/MainActivity.java
@@ -1,16 +1,62 @@
 package com.example.miranlee.spotmemo;
 
+import android.Manifest;
+import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
-import android.support.v7.app.ActionBarActivity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity implements TextToSpeech.OnInitListener{
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements com.google.android.gms.location.LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, PlaceSelectionListener, OnMapReadyCallback, TextToSpeech.OnInitListener{
 
     Button btn_myMemo;
     TextToSpeech tts;
+
+
+    GoogleMap map;
+    double latitude = 37.554752;
+    double longitude = 126.970631;
+
+    GoogleApiClient googleApiClient = null;
+
+    LocationManager locationManager;
+    //LocationListener locationListener;
+    LocationRequest locationRequest;
+    Location location;
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +67,43 @@ public class MainActivity extends ActionBarActivity implements TextToSpeech.OnIn
         btn_myMemo = (Button)findViewById(R.id.btn_myMemo);
         // 메모가 몇개인지 알아내서 버튼 이름 바꿔야지~
         tts = new TextToSpeech(this, this);
-        tts.speak("주변에 내가 남긴 N개의 메모가 있습니다",TextToSpeech.QUEUE_FLUSH,null);
+        //tts.speak("주변에 내가 남긴 N개의 메모가 있습니다",TextToSpeech.QUEUE_FLUSH,null);
+
+        int Fine_Perm_Check = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if(Fine_Perm_Check == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this,new String[]{
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                    android.Manifest.permission.INTERNET,
+                    android.Manifest.permission.ACCESS_NETWORK_STATE,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.RECORD_AUDIO
+            },0);
+        }
+        FragmentManager fragmentManager = getFragmentManager();
+        MapFragment mapFragment = (MapFragment)fragmentManager
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addApi(Places.GEO_DATA_API)
+                    .addApi(Places.PLACE_DETECTION_API)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API) //로케이션정보를 얻겠다
+                    .build();
+        }
+
+        locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10000)
+                .setFastestInterval(1000)
+                .setSmallestDisplacement(100);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
     }
 
     public void onClickMyMemo(View view) {
@@ -42,5 +124,82 @@ public class MainActivity extends ActionBarActivity implements TextToSpeech.OnIn
     @Override
     public void onInit(int i) {
 
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            int Fine_Perm_Check = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+            if(Fine_Perm_Check == PackageManager.PERMISSION_DENIED){
+                ActivityCompat.requestPermissions(this,new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET,
+                        Manifest.permission.ACCESS_NETWORK_STATE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                },0);
+            }
+            return;
+        }
+
+        location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        if(location != null)
+            updateMap(location);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient,locationRequest,this);
+
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location != null){
+            // Log.i("Location")
+            updateMap(location);
+        }
+    }
+
+    @Override
+    public void onPlaceSelected(Place place) {
+
+    }
+
+    @Override
+    public void onError(Status status) {
+        Toast.makeText(this, "Place selection failed: " + status.getStatusMessage(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+
+    public void updateMap(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        final LatLng Loc = new LatLng(latitude, longitude);
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(Loc, 16));
+
+        MarkerOptions options = new MarkerOptions();
+        options.position(Loc);
+        //options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker));
+        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+        options.title("내 위치");
+        options.snippet("내 위치");
+        map.addMarker(options);
+        //mylocation.setText(latitude+" , "+longitude);
     }
 }
